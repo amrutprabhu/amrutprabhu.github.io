1696bb87ed0caea39db252f4e89674379bb2169d
amrutprabhu <amrutprabhu42@gmail.com>
2021-04-29 15:17:09 +0200

    Updated tag manager

--- _includes/js/google-analytics.js
+++ _includes/js/google-analytics.js
@@ -1,9 +1,9 @@
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.googletagmanager.com/gtag/js?id=G-TJNP43P29P','ga');
+  })(window,document,'script','//www.googletagmanager.com/gtag/js?id={{ site.data.settings.google-ID }}','ga');
 
   window.dataLayer = window.dataLayer || [];
   function ga(){dataLayer.push(arguments);}
   ga('js', new Date());
-  ga('config', 'G-TJNP43P29P');
+  ga('config', '{{ site.data.settings.google-ID }}', { 'anonymize_ip': true, 'allowAdFeatures': false });
