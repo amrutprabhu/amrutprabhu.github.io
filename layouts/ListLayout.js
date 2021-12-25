76b986420d34fb8690a3a0e2417290c8cba7e20b
amrutprabhu <amrutprabhu42@gmail.com>
2021-12-25 15:56:20 +0100

    Initial changes

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -4,8 +4,15 @@ import siteMetadata from '@/data/siteMetadata'
 import { useState } from 'react'
 import Pagination from '@/components/Pagination'
 import formatDate from '@/lib/utils/formatDate'
+import Image from '@/components/Image'
 
-export default function ListLayout({ posts, title, initialDisplayPosts = [], pagination }) {
+export default function ListLayout({
+  posts,
+  title,
+  initialDisplayPosts = [],
+  authorDetails,
+  pagination,
+}) {
   const [searchValue, setSearchValue] = useState('')
   const filteredBlogPosts = posts.filter((frontMatter) => {
     const searchContent = frontMatter.title + frontMatter.summary + frontMatter.tags.join(' ')
@@ -15,7 +22,6 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
   // If initialDisplayPosts exist, display it if no searchValue is specified
   const displayPosts =
     initialDisplayPosts.length > 0 && !searchValue ? initialDisplayPosts : filteredBlogPosts
-
   return (
     <>
       <div className="divide-y">
@@ -50,7 +56,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
         <ul>
           {!filteredBlogPosts.length && 'No posts found.'}
           {displayPosts.map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
+            const { slug, date, title, summary, tags, imageUrl } = frontMatter
             return (
               <li key={slug} className="py-4">
                 <article className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
@@ -63,8 +69,16 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                   <div className="space-y-3 xl:col-span-3">
                     <div>
                       <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                        <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
-                          {title}
+                        <Link href={`/${slug}`} className="text-gray-900 dark:text-gray-100">
+                          <Image
+                            alt={title}
+                            src={imageUrl}
+                            className="object-cover object-center lg:h-58 md:h-36"
+                            width={744}
+                            height={306}
+                          />
+                          {/* TODO Formatting*/}
+                          <div>{title}</div>
                         </Link>
                       </h3>
                       <div className="flex flex-wrap">
@@ -75,6 +89,36 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                     </div>
                     <div className="prose text-gray-500 max-w-none dark:text-gray-400">
                       {summary}
+                    </div>
+                    <div>
+                      {authorDetails.map((author) => (
+                        <li className="flex items-center space-x-2" key={author.name}>
+                          {author.avatar && (
+                            <Image
+                              src={author.avatar}
+                              width="38px"
+                              height="38px"
+                              alt="avatar"
+                              className="w-10 h-10 rounded-full"
+                            />
+                          )}
+                          <dl className="text-sm font-medium leading-5 whitespace-nowrap">
+                            <dt className="sr-only">Name</dt>
+                            <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
+                            <dt className="sr-only">Twitter</dt>
+                            <dd>
+                              {author.twitter && (
+                                <Link
+                                  href={author.twitter}
+                                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                                >
+                                  {author.twitter.replace('https://twitter.com/', '@')}
+                                </Link>
+                              )}
+                            </dd>
+                          </dl>
+                        </li>
+                      ))}
                     </div>
                   </div>
                 </article>
--- pages/blog.js
+++ pages/blog.js
@@ -2,6 +2,7 @@ import { getAllFilesFrontMatter } from '@/lib/mdx'
 import siteMetadata from '@/data/siteMetadata'
 import ListLayout from '@/layouts/ListLayout'
 import { PageSEO } from '@/components/SEO'
+import { getFileBySlug } from '@/lib/mdx'
 
 export const POSTS_PER_PAGE = 5
 
@@ -13,10 +14,18 @@ export async function getStaticProps() {
     totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
   }
 
-  return { props: { initialDisplayPosts, posts, pagination } }
+  // TODO Imporove this
+  const authorList = ['default']
+  const authorPromise = authorList.map(async (author) => {
+    const authorResults = await getFileBySlug('authors', [author])
+    return authorResults.frontMatter
+  })
+  const authorDetails = await Promise.all(authorPromise)
+
+  return { props: { initialDisplayPosts, posts, pagination, authorDetails } }
 }
 
-export default function Blog({ posts, initialDisplayPosts, pagination }) {
+export default function Blog({ posts, initialDisplayPosts, pagination, authorDetails }) {
   return (
     <>
       <PageSEO title={`Blog - ${siteMetadata.author}`} description={siteMetadata.description} />
@@ -24,6 +33,7 @@ export default function Blog({ posts, initialDisplayPosts, pagination }) {
         posts={posts}
         initialDisplayPosts={initialDisplayPosts}
         pagination={pagination}
+        authorDetails={authorDetails}
         title="All Posts"
       />
     </>
