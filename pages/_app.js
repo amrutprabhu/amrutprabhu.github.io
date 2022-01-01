8e6e6960cb36d978d66c3912ee35c9525b8d6595
Amrut Prabhu <amrut.prabhu@raisin.com>
2022-01-01 15:10:22 +0100

    Added privacy settings

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,9 @@ import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+import ConsentWindow from '@/components/consentwindow'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
@@ -19,10 +19,10 @@ export default function App({ Component, pageProps }) {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
+      <ConsentWindow />
     </ThemeProvider>
   )
 }
