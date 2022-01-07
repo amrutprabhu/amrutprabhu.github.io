6ca50edb68b1c75e2d9f2fb45dd1927d43cd5ff0
Amrut Prabhu <amrut.prabhu@raisin.com>
2022-01-07 22:36:13 +0100

    Fixed logo

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,12 +1,12 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import Script from 'next/script'
+import Image from 'next/image'
 
 const LayoutWrapper = ({ children }) => {
   return (
@@ -14,13 +14,13 @@ const LayoutWrapper = ({ children }) => {
       <div className="flex flex-col justify-between h-screen">
         <header className="flex items-center justify-between py-10">
           <div>
-            <Link href="/" aria-label="Tailwind CSS Blog">
+            <Link href="/" aria-label="RefactorFirst">
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  <Image src="/static/favicons/new-icon.png" height={50} width={50} />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  <div className="h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
