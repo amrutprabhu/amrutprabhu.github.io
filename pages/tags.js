839479cbdda4d93cc9a88dde8b4b34787e4a09a2
Amrut Prabhu <amrut.prabhu@raisin.com>
2022-02-05 23:18:32 +0530

    Removed unused import

--- pages/tags.js
+++ pages/tags.js
@@ -1,7 +1,6 @@
 import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
 import Tag from '@/components/Tag'
-import siteMetadata from '@/data/siteMetadata'
 import { getAllTags } from '@/lib/tags'
 import kebabCase from '@/lib/utils/kebabCase'
 
