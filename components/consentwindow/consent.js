820a51471c693eb50dbd334c68c78339bb03d6c9
Amrut Prabhu <amrut.prabhu@raisin.com>
2022-07-08 11:30:02 +0200

    Fixed google analytics window

--- components/consentwindow/consent.js
+++ components/consentwindow/consent.js
@@ -6,7 +6,7 @@ import SectionContainer from '../SectionContainer'
 import classes from './consent.module.css'
 import siteMetadata from '@/data/siteMetadata'
 
-export function Consent() {
+export default function Consent() {
   // Not Used
   function acceptCookies() {
     Cookies.set('cookie-consent', true)
@@ -58,20 +58,20 @@ export function Consent() {
               return null;
           }
 
-          function addAnalytics(){
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','//www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}','ga');
+          // function addAnalytics(){
+          //   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          //     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          //     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          //     })(window,document,'script','//www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}','ga');
             
-              window.dataLayer = window.dataLayer || [];
-              function ga(){dataLayer.push(arguments);}
-              ga('js', new Date());
-              ga('config', '${siteMetadata.analytics.googleAnalyticsId}', { 'anonymize_ip': true, 'allowAdFeatures': false });
-          }
+          //     window.dataLayer = window.dataLayer || [];
+          //     function ga(){dataLayer.push(arguments);}
+          //     ga('js', new Date());
+          //     ga('config', '${siteMetadata.analytics.googleAnalyticsId}', { 'anonymize_ip': true, 'allowAdFeatures': false });
+          // }
 
           if(readCookie('cookie-notice-dismissed')=='true') {
-            addAnalytics()
+            // addAnalytics()
             document.getElementById('cookie-notice').style.display = 'none'
           } else {
               document.getElementById('cookie-notice').style.display = 'block';
