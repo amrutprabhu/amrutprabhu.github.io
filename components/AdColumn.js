563e4a34059737f356d4995537c98c7375084562
amrutprabhu <amrutprabhu42@gmail.com>
2023-01-26 20:22:47 +0100

    Adding changes

--- components/AdColumn.js
+++ components/AdColumn.js
@@ -4,15 +4,17 @@ import Link from '@/components/Link'
 function AdColumn({ width, height, imageLink, referalLink }) {
   return (
     <div className="border-4 border-teal-500 rounded-lg p-1">
-      <Link href={referalLink}>
-        <Image
-          alt="title"
-          src={`/${imageLink}`}
-          className="object-cover object-center "
-          width={width}
-          height={height}
-        />
-      </Link>
+      <div>
+        <Link href={referalLink}>
+          <Image
+            alt="title"
+            src={`/${imageLink}`}
+            className="object-cover object-center "
+            width={width}
+            height={height}
+          />
+        </Link>
+      </div>
     </div>
   )
 }
