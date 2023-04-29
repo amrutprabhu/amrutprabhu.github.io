30b9f3b26f0aefd13d9af8766b04ad4e4439edab
amrutprabhu <amrutprabhu42@gmail.com>
2023-04-29 17:50:53 +0200

    Fixed tags SEO title

--- pages/tags/[tag].js
+++ pages/tags/[tag].js
@@ -53,8 +53,8 @@ export default function Tag({ posts, authorDetails, tag }) {
   return (
     <>
       <TagSEO
-        title={`${tag} - ${siteMetadata.author}`}
-        description={`${tag} tags - ${siteMetadata.author}`}
+        title={`${tag[0].toUpperCase() + tag.split('-').join(' ').slice(1)}`}
+        description={`${tag[0].toUpperCase() + tag.split('-').join(' ').slice(1)} tags`}
       />
       <ListLayout posts={posts} authorDetails={authorDetails} title={title} />
     </>
