12a81dce34429b0d7e746094b79d62a4e6dc6041
amrutprabhu <amrutprabhu42@gmail.com>
2023-05-12 23:27:24 +0200

    Adding initial newsletter page version

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   // { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/posts', title: 'Posts' },
+  { href: '/newsletter', title: 'Newsletter' },
   { href: '/about', title: 'About' },
 ]
 
